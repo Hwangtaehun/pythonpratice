56929dcd0ebbf7a2c140a677f96fecf7a9a3d919
Hwangtaehun <r283@naver.com>
2023-08-29 20:56:31 +0900

    모듈 다루기

---
+++ modulepackage/main.py
@@ -0,0 +1,19 @@
+# import theater_module
+# import theater_module as mv
+# from theater_module import *
+# from theater_module import price, price_morning
+from theater_module import price_soldier as price
+
+# theater_module.price(3)
+# theater_module.price_morning(4)
+# theater_module.price_soldier(5)
+
+# mv.price(3)
+# mv.price_morning(4)
+# mv.price_soldier(5)
+
+# price(3)
+# price_morning(4)
+# price_soldier(5)
+
+price(5)
